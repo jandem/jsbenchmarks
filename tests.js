e1899d83cfcfc7f1f24d66e632ca9dcb703d2988
Jan de Mooij <jandemooij@gmail.com>
2013-11-30 00:56:17 +0100

    Other changes

--- tests.js
+++ tests.js
@@ -1,5 +1,6 @@
 var testList = [
     {filename:"number-tostring.htm", name:"number.toString()"},
-    {filename:"pixastic-desaturate.htm", name:"Pixastic desaturate"},
-    {filename:"JSAStarBenchmark/index.htm", name:"JSA* Benchmark"}
+    {filename:"pixastic-desaturate/index.htm", name:"Pixastic desaturate"},
+    {filename:"JSAStar/index.htm", name:"JSA* Benchmark"},
+    {filename:"bigint/index.htm", name:"jsbn BigInteger"},
 ];
