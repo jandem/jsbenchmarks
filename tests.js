4c92486bb50fe9957b9bf595542085285536054c
Jan de Mooij <jandemooij@gmail.com>
2013-11-30 01:07:35 +0100

    Add distance transform test

--- tests.js
+++ tests.js
@@ -3,4 +3,5 @@ var testList = [
     {filename:"pixastic-desaturate/index.htm", name:"Pixastic desaturate"},
     {filename:"JSAStar/index.htm", name:"JSA* Benchmark"},
     {filename:"bigint/index.htm", name:"jsbn BigInteger"},
+    {filename:"distance-transform.htm", name:"Distance Transform"},
 ];
